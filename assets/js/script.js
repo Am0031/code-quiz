e9d0bf7c4dab23f404a9200675ae5655369cd152
Amelie Pira <amelie.pira@gmail.com>
2022-04-27 21:57:37 +0100

    cleaned console.log in onLoad

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,13 +172,12 @@ const onLoad = () => {
   // function to initialise local storage
   //get info from local storage
   const highScoresFromLS = JSON.parse(localStorage.getItem(localStorageKey));
-  console.log(highScoresFromLS);
+
   // check if highscores exists in LS
   //if it doesn't exist, set an empty array and stringify as we set it
   if (!highScoresFromLS) {
     localStorage.setItem(localStorageKey, JSON.stringify([]));
   }
-  console.log(JSON.parse(localStorage.getItem(localStorageKey)));
 };
 
 const startTimer = () => {
